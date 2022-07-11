2d6c55acf36a98cce6fce522ab949ce24d0f2c17
perkles <suelane.vieira@gmail.com>
2022-07-11 16:34:13 -0300

    commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -22,6 +22,18 @@ public class PrimeiroPrograma {
 class Livros {
 	private String nome;
 	private String npag;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNpag() {
+		return npag;
+	}
+	public void setNpag(String npag) {
+		this.npag = npag;
+	}
 	
 	
 }
